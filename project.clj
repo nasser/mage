a9b8b66c506adce31d922bb09d6b29d868be18f6
Ramsey Nasser <ram@nas.sr>
2017-08-26 22:10:56 -0400

    Make lein compatible

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nasser/mage "0.0.2-SNAPSHOT"
+  :description "Symbolic MSIL bytecode generation for ClojureCLR"
+  :url "https://github.com/nasser/mage"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
